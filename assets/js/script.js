0c4ffc9ea8be7072386fa79877ccd1b82baa56e1
adesuryadi587 <adesuryadi587@gmail.com>
2022-06-23 18:39:51 +0700

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,42 @@
+$( document ).ready(function() {
+    //ambil elemen id
+    var mybutton = document.getElementById("myBtn");
+
+    // tampilkan button jika scroll > 100
+    window.onscroll = function () { scrollFunction() };
+
+    function scrollFunction() {
+        if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+            mybutton.style.display = "block";
+        } else {
+            mybutton.style.display = "none";
+        }
+    }
+
+    $('a[href*="#"]').click(function(event) {
+        if (
+            location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
+            location.hostname == this.hostname
+        ) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                event.preventDefault();
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000, function() {
+                    var $target = $(target);
+                    $target.focus();
+                    if ($target.is(":focus")) {
+                        return false;
+                    } else {
+                        $target.attr('tabindex', '-1');
+                        $target.focus();
+                    };
+                });
+            }
+        }
+    });
+    
+});
+
